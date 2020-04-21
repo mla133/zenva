588cadd7f02a54d436b78e1fc44e273fbea43b9a
Matt Allen <matthew.allen@technipfmc.com>
2020-04-21 17:13:37 -0400

    Updated UI, added enemies and interactions with objects.  Set collision masks on scenery.

--- Godot2DRPG/Player.gd
+++ Godot2DRPG/Player.gd
@@ -19,7 +19,14 @@ var facingDir : Vector2 = Vector2()
 
 onready var rayCast = get_node("RayCast2D")
 onready var anim = get_node("AnimatedSprite")
+onready var ui = get_node("/root/MainScene/CanvasLayer/UI")
 
+func _ready():
+	ui.update_level_text(curLevel)
+	ui.update_health_bar(curHp, maxHp)
+	ui.update_xp_bar(curXp, xpToNextLevel)
+	ui.update_gold_text(gold)
+	
 func _physics_process(delta):
 	vel = Vector2()
 	
@@ -62,17 +69,56 @@ func manage_animations():
 		play_animation("IdleUp")
 	elif facingDir.y == 1:
 		play_animation("IdleDown")
-	
 		
+func _process(delta):
+	if Input.is_action_just_pressed("interact"):
+		try_interact()
+
+func try_interact():
+	rayCast.cast_to = facingDir * interactDist
 	
+	if rayCast.is_colliding():
+		if rayCast.get_collider() is KinematicBody2D:
+			rayCast.get_collider().take_damage(damage)
+		elif rayCast.get_collider().has_method("on_interact"):
+			rayCast.get_collider().on_interact(self)
 	
 func play_animation(anim_name):
 	
 	if anim.animation != anim_name:
 		anim.play(anim_name)
 		
-		
-		
+func give_xp(amount):
+	curXp += amount
 	
+	ui.update_xp_bar(curXp, xpToNextLevel)
+	
+	if curXp >= xpToNextLevel:
+		level_up()
+		
+func level_up():
+	var overflowXp = curXp - xpToNextLevel
+	
+	xpToNextLevel *= xpToLevelIncreaseRate
+	curXp = overflowXp
+	
+	curLevel += 1
+	
+	ui.update_xp_bar(curXp, xpToNextLevel)
+	ui.update_level_text(curLevel)
+		
+func take_damage(dmgToTake):
+	curHp -= dmgToTake
+	ui.update_health_bar(curHp, maxHp)
+	
+	if curHp <= 0:
+		die()
+		
+func die():
+	get_tree().reload_current_scene()
+		
+func give_gold(amount):
+	gold += amount
+	ui.update_gold_text(gold)
 	
 	
